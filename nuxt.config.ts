6e339aae1bb96fdd77f7fde8c5f5e4dd50e7f9cf
FreddyCro <freddy10100@hotmail.com>
2026-05-21 18:18:25 +0800

    chore: updated env and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+
+  // 讓區網設備可以使用，例如手機
+  // devServer: {
+  //   host: "0.0.0.0",
+  //   port: 3000,
+  // },
+
+  ssr: true,
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      failOnError: false,
+    },
+  },
+
+  runtimeConfig: {
+    public: {
+      APP_MODE: "",
+      APP_ASSETS_PATH: "",
+    },
+  },
+
+  app: {
+    baseURL: (() => {
+      const nuxtUrl = process.env.NUXT_URL;
+      if (!nuxtUrl) return "/";
+      try {
+        return new URL(nuxtUrl).pathname;
+      } catch {
+        return "/";
+      }
+    })(),
+  },
+});
